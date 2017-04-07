264b4e1b8b1d84aa5fbc357b90fbf5ca805b3381
mattg317 <mattg317@gmail.com>
2017-04-07 15:41:01 -0400

    minor folder re structure

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.conf['SQLALCHEMY_DATABASE_URI']
+db =SQLAlchemy(app)
 
 @app.route('/')
 def index():
